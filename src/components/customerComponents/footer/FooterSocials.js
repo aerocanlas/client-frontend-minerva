be295459f188d45ac1eefc156ea00f01cc0de67e
Kyla <kylasingian@gmail.com>
2023-11-12 16:50:07 +0800

    footer socials changed

--- src/components/customerComponents/footer/FooterSocials.js
+++ src/components/customerComponents/footer/FooterSocials.js
@@ -1,27 +1,13 @@
 import React from "react";
-import Github from "../../../assets/images/socials/github.svg";
-import Linkedin from "../../../assets/images/socials/linkedin.svg";
-import Website from "../../../assets/images/socials/portfolio.png";
+import Facebook from "../../../assets/images/socials/facebook.svg";
 
 const socials = [
   {
-    id: 1,
-    href: "https://github.com/catherineisonline/pizza-time-with-react",
-    img: Github,
-    name: Github
-  },
-  {
     id: 2,
-    href: "https://www.linkedin.com/in/catherinemitagvaria/",
-    img: Linkedin,
-    name: Linkedin
+    href: "https://www.facebook.com/MinervaSalesCorp",
+    img: Facebook,
+    name: Facebook
   },
-  {
-    id: 3,
-    href: "https://ekaterine-mitagvaria.vercel.app/",
-    img: Website,
-    name: Website
-  }
 ];
 
 export default class FooterSocials extends React.Component {
@@ -37,7 +23,7 @@ export default class FooterSocials extends React.Component {
             aria-label={social.name}
           >
           <span aria-hidden="true">{social.name}</span>
-            <img src={social.img} alt="" aria-hidden="true" />
+            <img src={social.img} alt="" aria-hidden="true"/>
           </a>
         </li>)}
       </ul>
--- src/data/ourServices.js
+++ src/data/ourServices.js
@@ -25,6 +25,20 @@ const ourServices = [
         description:
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dictum eget ligula a malesuada.',
     },
+    {
+        id: 3,
+        img: ImgThree,
+        name: 'Service 3',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dictum eget ligula a malesuada.',
+    },
+    {
+        id: 3,
+        img: ImgThree,
+        name: 'Service 3',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dictum eget ligula a malesuada.',
+    },
 ]
 
 export default ourServices;
